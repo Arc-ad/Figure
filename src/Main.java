9742cea9e0e310f584516e3cc047ac1feaa88565
Arc-ad <barkovskiia@bk.ru>
2022-05-30 16:38:52 +0300

    asd

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
     public static void main(String[] args) {
         Circle circle = new Circle("Окружность", 5);
         System.out.println(circle.getName() + " с радиусом: " + circle.r);
-
+        System.out.println();
     }
 }
